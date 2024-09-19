b90c8b37f07c393545b57a3234970ed6f5883fed
kingboburshoh <kingboburshoh@gmail.com>
2024-09-19 08:33:21 +0500

    desktop version completed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let dateEl=document.querySelectorAll("#news-card-date");
 const notificationBtn=document.querySelector(".notification-btn")
-let currentDate= new Date().toLocaleString()
+let currentDate= new Date().toLocaleString();
+const subscribeBtn=document.querySelector(".subscribe-btn")
 
 setInterval(justFunc,1);
 
@@ -9,7 +10,10 @@ function justFunc() {
     el.innerHTML=currentDate
    });
 }
-
+subscribeBtn.addEventListener("click",(e)=>{
+   window.location.assign("https://t.me/my_first_blog_IT")
+   
+})
 notificationBtn.addEventListener("click",()=>{
    alert("Reklamalar yoqildi !.")
 })
